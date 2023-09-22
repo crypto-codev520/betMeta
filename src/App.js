26aa5571ae7b57ace03458053cf764bca0d423ac
crypto-codev520 <kyotomo920@gmail.com>
2023-09-22 01:22:49 -0400

    update n issues

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import Landing from './components/Layout/Landing';
 import PageTemplate from './components/Layout/sideNavbar';
 import Buy from './components/Layout/Buy';
+import Profile from './components/Layout/Profile';
 import './App.css';
 
 import {BrowserRouter as Router, Route, Routes} from 'react-router-dom'
@@ -14,6 +15,8 @@ function App() {
         <Routes>
           <Route exact path="/" element={<Landing></Landing>}></Route>
           <Route exact path="/buy" element={<Buy></Buy>}></Route>
+          <Route exact path="/profile" element={<Profile></Profile>}></Route>
+
 
         </Routes>
       </Router>
